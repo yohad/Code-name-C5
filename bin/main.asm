f939fec49eb07057bc8b3e40f34432761297e39d
Itamar Abeliovich <iabeliovich@gmail.com>
2016-12-20 23:40:22 +0200

    Fixed the trap read/write mechanism to use string instructions. Optimization may be required. Also added a shellscript to copy the assembled com file from the bin directory to the game engine's survivors directory, and then starts the game. Use it after assembling. Currently it duplicates the file into two team members, but this can be changed later.

--- bin/main.asm
+++ bin/main.asm
@@ -1,23 +1,21 @@
-IDEAL
+DEAL
 model tiny
 CODESEG
 org 100h
 
-TRAP_SRC equ 0		; The start address in private memory where the trap is stored.
-TRAP_SIZE equ 11	; The size of the trap in decimal.
+TRAP_SRC equ 0		; The location in shared memory where the trap is stored.
+TRAP_SIZE equ trap_data_end - trap_data	; The size of the trap in bytes.
 
 start:
+	mov di, 0		; This block stores the trap instructions in private memory.
+	mov si, offset trap_data
+	mov cx, TRAP_SIZE
+	rep movsb 
+	
 	mov di, ax
 	add di, (exit - start)
-	mov ax, 20h
-
-	mov [byte ptr es:TRAP_SRC], 0BBh
-	mov [word ptr es:TRAP_SRC + 1], 0000h	; Placeholder. Each time the trap is placed this value needs to be changed to the current trap's IP.
-	mov [word ptr es:TRAP_SRC + 3], 81C3h
-	mov [word ptr es:TRAP_SRC + 5], 8000h
-	mov [word ptr es:TRAP_SRC + 7], 0C607h
-	mov [word ptr es:TRAP_SRC + 9], 0CCCCh
-
+	mov dx, 3Fh
+	
 	;;;TRAP;;;
 	;mov bx, {IP of trap}
 	;add bx, 8000h
@@ -25,22 +23,38 @@ start:
 	;int 3
 	;;;END;;;
 
+trap:
+	xor si, si	; Prepares to move the trap contents from ds:[0 -> 10] to es:[di -> di+10]
+	add di, dx
+	mov ax, di
+	mov di, TRAP_SRC + 1
+	stosw
+	mov di, ax
+	;mov bx, {location of trap start}
+	; Updates the trap code to use the current trap's starting address.
+		
 	push ds
 	push es
 	pop ds
 	pop es	; DS and ES must be swapped for movs to copy private memory to public memory.
-trap:
-	xor si, si	; Prepares to move the trap contents from ds:[0 -> 10] to es:[di -> di+10]
-	add di, ax
-	mov ds:[1], di
-	;mov bx, {location of trap start}
-	; Updates the trap code to use the current trap's starting address.
-	
+
 	mov cx, TRAP_SIZE
 	rep movsb
+	
+	push ds
+	push es
+	pop ds
+	pop es
 
-	add ax, ax
-	neg ax
+	add dx, dx
+	neg dx
+
 	jmp trap
 exit:
+trap_data:
+	mov bx, 1111h	; Placeholder data
+	add bx, 8000h
+	mov [byte ptr bx], 0CCh
+	int 3
+trap_data_end:
 END start
---
+++ prep.sh
@@ -0,0 +1,4 @@
+cp ./bin/main.com ./corewars_engine/survivors/main1
+cp ./bin/main.com ./corewars_engine/survivors/main2
+cd corewars_engine
+java -cp ./lib/corewars8086-4.0.1.jar il.co.codeguru.corewars8086.CoreWarsEngine
