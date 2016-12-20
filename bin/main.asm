0c27471807efe469336d377a0cb06798dd9f6e72
Itamar Abeliovich <iabeliovich@gmail.com>
2016-12-20 13:25:57 +0200

    The trap is now stored in private memory and movsb is used to compactly copy it to the destination each time. NOTE: After the setup phase, ES points to public memory and DS points to private memory.

--- bin/main.asm
+++ bin/main.asm
@@ -2,29 +2,44 @@ IDEAL
 model tiny
 CODESEG
 org 100h
+
+TRAP_SRC equ 0		; The start address in private memory where the trap is stored.
+TRAP_SIZE equ 11	; The size of the trap in decimal.
+
 start:
-	mov bx, ax
-	add bx, (exit - start)
+	mov di, ax
+	add di, (exit - start)
 	mov ax, 20h
+
+	mov byte ptr es:[TRAP_SRC], 0BBh
+	mov word ptr es:[TRAP_SRC + 1], 0000h	; Placeholder. Each time the trap is placed this value needs to be changed to the current trap's IP.
+	mov word ptr es:[TRAP_SRC + 3], 81C3h
+	mov word ptr es:[TRAP_SRC + 5], 8000h
+	mov word ptr es:[TRAP_SRC + 7], 0C607h
+	mov word ptr es:[TRAP_SRC + 9], 0CCCCh
+
+	;;;TRAP;;;
+	;mov bx, {IP of trap}
+	;add bx, 8000h
+	;mov byte ptr [bx], 0CCh
+	;int 3
+	;;;END;;;
+
+	mov bx, ds
+	mov cx, es
+	mov ds, cx
+	mov es, bx	; Swap ds and es for strcpy to operate from private memory to public memory.
+
 trap:
-		;;;TRAP;;;
-		;mov bx, {IP of trap}
-		;add bx, 8000
-		;mov byte ptr [bx], 0CCh
-		;int 3
-		;;;END;;;
-	add bx, ax
-		;mov bx, {location of trap start}
-	mov [byte bx], 0BBh
-	mov [word bx+1], bx
-		;add bx, 8000 --To get to the other side of the arena
-	mov [word bx+3], 81C3h
-	mov [word bx+5], 8000h
-		;mov byte ptr [bx], 0CCh
-	mov [word bx+7], 0C607h
-	mov [byte bx+8], 0CCh
-		;kill the poor soul
-	mov [byte bx+9], 0CCh
+	xor si, si	; Prepares to move the trap contents from ds:[0 -> 10] to es:[di -> di+10]
+	add di, ax
+	mov word ptr ds:[1], di
+	;mov bx, {location of trap start}
+	; Updates the trap code to use the current trap's starting address.
+	
+	mov cx, TRAP_SIZE
+	rep movsb
+
 	add ax, ax
 	neg ax
 	jmp trap
