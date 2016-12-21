4b032675ca13a410f0ac61035164fd14eb4ed811
Itamar Abeliovich <iabeliovich@gmail.com>
2016-12-21 23:39:41 +0200

    Eliminated the need for segment swapping with the stack by inserting the current trap's address while writing it.

--- bin/main.asm
+++ bin/main.asm
@@ -22,31 +22,23 @@ start:
 	;mov byte ptr [bx], 0CCh
 	;int 3
 	;;;END;;;
-
-trap:
-	xor si, si	; Prepares to move the trap contents from ds:[0 -> 10] to es:[di -> di+10]
-	add di, dx
-	mov ax, di
-	mov di, TRAP_SRC + 1
-	stosw
-	mov di, ax
-	;mov bx, {location of trap start}
-	; Updates the trap code to use the current trap's starting address.
 		
 	push ds
 	push es
 	pop ds
 	pop es	; DS and ES must be swapped for movs to copy private memory to public memory.
 
-	mov cx, TRAP_SIZE
+trap:
+	xor si, si	; Prepares to move the trap contents from ds:[0 -> 10] to es:[di -> di+10]
+	add di, dx
+	mov ax, di	; ax will be used with stosw to store the trap's starting address in the trap later on.
+
+	movsb
+	stosw
+	mov cx, TRAP_SIZE-3
 	rep movsb
 	sub di, TRAP_SIZE
 	
-	push ds
-	push es
-	pop ds
-	pop es
-
 	mov ax, dx
 	add ax, ax
 	add dx, ax
