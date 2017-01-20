7c62f7fa64092628c1017068e55a9aae9b4a49dc
Yotam Ohad <yotam@ubimo.com>
2017-01-20 12:02:17 +0200

    dunno

--- bin/iCopy.asm
+++ bin/iCopy.asm
@@ -3,44 +3,42 @@ global _start
 
 _start:
 
-    mov     bx, ax
-    add     bx, (_exit - start)
-    ;Load the acopier to ES
-    mov     si, _copier
-    mov     cx, (_trap - _copier) / 2
-    rep     movsw
-    ;Load NOPs untill the trap to ES
-    mov     ax, 9090
-    mov     cx, (1024 - (_exit - _copier)) / 2
+    push    ax
+    mov     si, ax
+
+    mov     cx, (0x400 - (_exit - _trap)) / 2
+    mov     ax, 0x9090
     rep     stosw
-    ;Load trap to ES
-    mov     si, _trap
+
+    add     si, (_trap - _start)
     mov     cx, (_exit - _trap) / 2
     rep     movsw
-    ;Replace ES and DS to copy from ES to the arena
+
     push    es
     push    ds
     pop     es
     pop     ds
-    ;Set direction flag for reverse copying
-    std
 
-    mov     di, bx
+    pop     di
+    add     di, (_exit - _start)
 
-_copier:
-
-    mov     si, 1024
-    add     di, 1024
-    mov     cx, 512
-    rep     movsw
-    dw      0xE9FD ;jmp relative to +1024 bytes
-    db      0x03
+    jmp     _main
 
 _trap:
 
+    nop
     std
-    mov     ax, 0xFACC
-    mov     cx, 512 
+    mov     ax, 0x9090
+    mov     cx, 0x200
     rep     stosw
+    int     0x3
+
+_main:
+
+    xor     si, si
+    mov     cx, 0x200
+    rep     movsw
+    ; add     di, 0x400
+    jmp     _main + 0x400
 
 _exit:
--- fighters/iRace.asm
+++ fighters/iRace.asm
@@ -4,8 +4,9 @@ global _start
 _start:
 
     mov     si, ax
-    add     si, (_exit - _main)
-    mov     cx, (_exit - _main) / 2
+    add     si, (_main - _start) - 2
+    mov     cx, (_exit - _main) / 2 + 2
+
     rep     movsw
 
     std
@@ -13,14 +14,20 @@ _start:
     push    ds
     pop     es
     pop     ds
-    mov     di, _start
+
+    mov     di, ax
+    jmp     _run
 
 _main:
+
+    int     0x3
+
+_run:
 
     mov     si, 0x400
     mov     cx, 0x200
     rep     movsw
-    sub     di, (_main - _start)
-    jmp     _start - 0x400
+    sub     di, _main - _start
+    jmp     _run - 0x400 - (_main - _start) + 1
 
 _exit:
