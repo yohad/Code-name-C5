99c7520cc729f032a6827c31316458edb890a7f5
yohad <yotam2206@gmail.com>
2017-02-22 01:44:24 +0200

    Bye Bye tabs

--- fighters/perpetual_imp1.asm
+++ fighters/perpetual_imp1.asm
@@ -1,173 +1,173 @@
 section _text
 global start
-LENGTH_CHECK 	equ 0x50
-SCAN_LENGTH		equ 0x79
-SCANBRANCH_LENGTH 	equ 128
+LENGTH_CHECK    equ 0x50
+SCAN_LENGTH     equ 0x79
+SCANBRANCH_LENGTH   equ 128
 
 start:
-	mov 	si, ax					;mov si, the location of the code
-	add 	si, (imp - start)		;make si point to the start of the imp
-	mov 	cx, (exit - imp)    	;put in cx number for rep (divided by 2 for word action)
-	rep 	movsb					;load to private memory the imp code
+    mov     si, ax                  ;mov si, the location of the code
+    add     si, (imp - start)       ;make si point to the start of the imp
+    mov     cx, (exit - imp)        ;put in cx number for rep (divided by 2 for word action)
+    rep     movsb                   ;load to private memory the imp code
 
-	push 	es						;exchange es and ds values for copying from private memory
-	push 	ds
-	pop 	es
-	pop 	ds
+    push    es                      ;exchange es and ds values for copying from private memory
+    push    ds
+    pop     es
+    pop     ds
 
-	mov 	di, ax					;di points to start
-	add 	di, (imp - start)		;di points to imp
-	xor 	si, si
-	mov 	cx, SCAN_LENGTH
-	mov 	dx, es 					; dx points to code/data segment
-	mov 	bx, ds 					; bx points to private memory
-	mov 	bp, 0x10
+    mov     di, ax                  ;di points to start
+    add     di, (imp - start)       ;di points to imp
+    xor     si, si
+    mov     cx, SCAN_LENGTH
+    mov     dx, es                  ; dx points to code/data segment
+    mov     bx, ds                  ; bx points to private memory
+    mov     bp, 0x10
 
-	movsw
+    movsw
 imp:
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	mov 	ds, dx
-	mov 	ax, [di+LENGTH_CHECK]
-	mov 	ds,bx
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    mov     ds, dx
+    mov     ax, [di+LENGTH_CHECK]
+    mov     ds,bx
 
-	movsw
-	movsw
-	not 	ax
+    movsw
+    movsw
+    not     ax
 
-	movsw
-	movsw
-	and 	ax, bp
+    movsw
+    movsw
+    and     ax, bp
 
-	movsw
-	movsw
-	movsw
-	movsw
-	mov ah, 8
-	mul ah
+    movsw
+    movsw
+    movsw
+    movsw
+    mov ah, 8
+    mul ah
 
-	movsw
-	movsw
-	add 	si, ax
+    movsw
+    movsw
+    add     si, ax
 
-	movsw							; post-comparison load
+    movsw                           ; post-comparison load
 
 scan:
 
-	movsw
-	mov 	al, 0xA5
+    movsw
+    mov     al, 0xA5
 
-	movsw
-	movsw
-	movsw
-	mov cx, SCAN_LENGTH
+    movsw
+    movsw
+    movsw
+    mov cx, SCAN_LENGTH
 
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	push 	di						; This is used to jump to the movsw later.
-	push 	si
-	push 	di
-	repne 	scasb
-	pop 	di
-	pop 	si
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    push    di                      ; This is used to jump to the movsw later.
+    push    si
+    push    di
+    repne   scasb
+    pop     di
+    pop     si
 
-	movsw
-	push 	cx
+    movsw
+    push    cx
 
-	movsw
-	movsw
-	neg		cx
+    movsw
+    movsw
+    neg     cx
 
-	movsw
-	movsw
-	xor 	ax, ax
+    movsw
+    movsw
+    xor     ax, ax
 
-	movsw
-	movsw
-	shl 	cx, 1
+    movsw
+    movsw
+    shl     cx, 1
 
-	movsw
-	movsw
-	adc		al, 0
+    movsw
+    movsw
+    adc     al, 0
 
-	movsw
-	movsw
-	movsw
-	movsw
-	mov 	ah, 0x10
-	mul		ah
+    movsw
+    movsw
+    movsw
+    movsw
+    mov     ah, 0x10
+    mul     ah
 
-	movsw
-	movsw
-	add 	si, ax
-	movsb							; Load next branch
+    movsw
+    movsw
+    add     si, ax
+    movsb                           ; Load next branch
 
 no_movsw_found:
-	movsw
-	movsw
-	pop 	ax						; Restore the stack to its previous state.
-	pop		ax
+    movsw
+    movsw
+    pop     ax                      ; Restore the stack to its previous state.
+    pop     ax
 
-	movsw
-	movsw
-	xor 	si, si
-	movsw
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
+    movsw
+    movsw
+    xor     si, si
+    movsw
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 
 movsw_found:
-	movsw
-	movsw
-	pop 	cx
-	pop 	ax 						; Start of scan address is stored in AX
+    movsw
+    movsw
+    pop     cx
+    pop     ax                      ; Start of scan address is stored in AX
 
-	movsw
-	movsw
-	neg		cx
+    movsw
+    movsw
+    neg     cx
 
-	movsw
-	movsw
-	movsw
-	movsw
-	add		cx, SCAN_LENGTH-1
-	movsw
-	movsw
-	add 	cx, ax
+    movsw
+    movsw
+    movsw
+    movsw
+    add     cx, SCAN_LENGTH-1
+    movsw
+    movsw
+    add     cx, ax
 
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	movsw
-	mov		di, cx
-	inc		di
-	xor 	si, si
-	jmp 	cx
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    movsw
+    mov     di, cx
+    inc     di
+    xor     si, si
+    jmp     cx
 scanbranch_end:
-	times	(SCANBRANCH_LENGTH - scanbranch_end + scan) db	0x90	; Padding
+    times   (SCANBRANCH_LENGTH - scanbranch_end + scan) db  0x90    ; Padding
 
-	movsw
-	xor si, si
-	movsw
+    movsw
+    xor si, si
+    movsw
 
-; 	movsw
+;   movsw
 ; post_scan:
-; 	movsw
-; 	xor 	si, si
-; 	movsw
+;   movsw
+;   xor     si, si
+;   movsw
 exit:
